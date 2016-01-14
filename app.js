9567c792c5191d567bcaed482bbf18f46df88684
Shannon Sullivan <shsulliv@gmail.com>
2016-01-14 10:59:39 +0100

    Added total hours feature

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+var $ = require('jquery');
+
 var h2 = document.getElementsByTagName('h2')[0],
     list = document.getElementsByTagName('ul')[0],
     start = document.getElementById('start'),
     stop = document.getElementById('stop'),
     done = document.getElementById('done'),
+    total = document.getElementById('total'),
     seconds = 0,
     minutes = 0,
     hours = 0,
@@ -21,12 +24,27 @@ function countUp() {
     }
   }
 
-  time = h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+  time = h2.textContent = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0'+ seconds);
 }
 
 function timer() {
   interval = setInterval(countUp, 1000);
 }
+
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  var values = $('form').serializeArray();
+  var valuesObj = {};
+  console.log(values)
+  var taskInfo = values.reduce(function(prev, curr) {
+    return prev + ' ' + curr.name + ': ' + curr.value;
+  },'');
+  var li = document.createElement('li');
+  li.textContent = taskInfo + ' time: ' + time;
+  $(li).data('task', {})
+  list.appendChild(li);
+  $('input').val('');
+});
 
 start.onclick = function() {
   timer();
@@ -37,12 +55,16 @@ stop.onclick = function() {
 }
 
 done.onclick = function() {
-  h2.textContent = "00:00:00";
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
+  $('form').submit();
+  clearInterval(interval);
+  h2.textContent = '00:00:00';
+  // seconds = 0;
+  // minutes = 0;
+  // hours = 0;
+}
 
-  var li = document.createElement('li');
-  li.textContent = time;
-  list.appendChild(li);
+total.onclick = function() {
+  console.log($('li'));
+  // $('section').append('<p>Total hours: ' + hours + '</p>');
+  // console.log(parseInt(time, 10));
 }
