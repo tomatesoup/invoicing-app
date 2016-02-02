fa831140a7e29cc147043bc828f9d620ce3f87af
Shannon Sullivan <shsulliv@gmail.com>
2016-02-02 14:37:41 +0100

    Created time object, made total button functional

--- app.js
+++ app.js
@@ -32,38 +32,50 @@ function countUp() {
       hours ++;
     }
   }
+  time = { hours: hours, minutes: minutes, seconds: seconds };
+  h2.textContent = formatTime(time); 
+}
 
-  time = h2.textContent = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':' + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':' + (seconds > 9 ? seconds : '0'+ seconds);
+function formatTime(t) {
+  t = (t.hours ? (t.hours > 9 ? t.hours : '0' + t.hours) : '00') + ':' + (t.minutes ? (t.minutes > 9 ? t.minutes : '0' + t.minutes) : '00') + ':' + (t.seconds > 9 ? t.seconds : '0'+ t.seconds);  
+  return t;
 }
 
 function timer() {
   interval = setInterval(countUp, 1000);
 }
 
-function createTaskItem (values) {
-  var taskInfo = _.map(values, function(v,k) { return k + ': ' + v }).join(' ');
+function createTaskItem(values) {
+  var taskInfo = _.map(values, function(v,k) {
+    if (k === 'time') {
+      v = formatTime(time);
+    } 
+    return k + ': ' + v 
+    }).join(' ');
   var li = document.createElement('li');
-  li.textContent = taskInfo + ' time: ' + time; 
+  li.textContent = taskInfo; 
   return li;
 }
 
-function render (s) {
+function update(s) {
   list.innerHTML = '';
-  _.forEach(_.map(s.tasks, createTaskItem), function (element) {
-    list.appendChild(element)
+  _.forEach(_.map(s.tasks, createTaskItem), function(element) {
+    list.appendChild(element);
   });
 }
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   clearInterval(interval);
+  var taskObj = serialize(form, true);
+  taskObj.time = time;
+  state.tasks.push(taskObj);
+  update(state);
+  form.reset();
   h2.textContent = '00:00:00';
   seconds = 0;
   minutes = 0;
   hours = 0;
-  state.tasks.push(serialize(form, true));
-  render(state);
-  form.reset();
 });
 
 start.onclick = function() {
@@ -74,7 +86,13 @@ stop.onclick = function() {
   clearInterval(interval);
 }
 
+function displayTotal(total) {
+  var p = document.createElement('p');
+  p.textContent = 'Total hours: ' + total;
+  return p;
+}
+
 total.onclick = function() {
-  // console.log($('li'));
-  // $('section').append('<p>Total hours: ' + hours + '</p>');
+  var totalHours = _.reduce(state.tasks, function(task) { return task.time.hours } );
+  list.appendChild(displayTotal(totalHours));
 }
